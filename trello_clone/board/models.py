b947eacee87d688525cc296b2536a4ef29308648
manjaka <manjaka@kali.kali>
2025-06-17 12:29:55 +0300

    feat(models): add Board, List and Card models with migrations

--- trello_clone/board/models.py
+++ trello_clone/board/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Board(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class List(models.Model):
+    name = models.CharField(max_length=225)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="list")
+    position = models.IntegerField(default=0) # pour l'ordre des listes
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Card(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    list = models.ForeignKey(List, on_delete=models.CASCADE, related_name="cards")
+    position = models.IntegerField(default=0)  # pour l'ordre des cartes
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
